f296b19c21f466ee0db0358119afc4584fdbc8b1
sgvozdev <sgvozdev@web-bee.ru>
2024-02-26 09:43:59 +0400

    Добавлен примитивный тест получения пользователя по id

--- test_api/test_list_users.py
+++ test_api/test_list_users.py
@@ -6,3 +6,10 @@ class TestUsers:
     def test_get_user_list(self):
         response = requests.get('https://reqres.in/api/users?page=2').json()
         assert response['page'] >= 1
+
+    def test_get_single_user(self):
+        response = requests.get('https://reqres.in/api/users/5')
+        response_json = response.json()
+        f = response_json['data']
+        assert response.status_code == 200
+        assert f['email'] == 'charles.morris@reqres.in'
