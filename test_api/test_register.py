b7283e0f124d4179308c10e2e280265d1cd40e94
GvozdevSV <agasfen82@yandex.ru>
2024-03-01 08:08:02 +0400

    Добавлен примитивный тест не корректной регистрации

--- test_api/test_register.py
+++ test_api/test_register.py
@@ -10,3 +10,11 @@ class TestRegister:
         }
         response = requests.post('https://reqres.in/api/register', json=payload)
         assert response.status_code == 200
+
+    def test_unsuccessful_register(self):
+        payload = {
+            "email": "eve.holt@reqres.in",
+        }
+        response = requests.post('https://reqres.in/api/register', json=payload)
+        assert response.status_code == 400
+        assert response.json()['error'] == 'Missing password'
